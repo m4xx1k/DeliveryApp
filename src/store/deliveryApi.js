996eb73dbe81df036d610b4479a4661d3883ecf8
m4xx1k <maxikfabin@gmail.com>
2022-07-10 03:52:00 +0300

    3511007

--- src/store/deliveryApi.js
+++ src/store/deliveryApi.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const deliveryApi = createApi({
     reducerPath: "deliveryApi",
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000/"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://delivery-app-max-server.herokuapp.com/"}),
 
     endpoints: (build) => ({
         addOrder: build.mutation({
